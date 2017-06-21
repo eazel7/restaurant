fdda96cca69649ce33d556004b2b0cc9663b2570
Diego Pérez <eazel7@gmail.com>
2017-06-21 13:25:58 -0300

    customers api

--- api/customers.js
+++ api/customers.js
@@ -1,6 +1,53 @@
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
 function Customers(db, bus) {
     this.customers = db.collection('customers');
     this.bus = bus;
+}
+
+Customers.prototype.search = function (filter) {
+    if (!filter) return Promise.reject(new Error('filter is required'));
+
+    return new Promise(
+        (resolve, reject) => {
+            var regex = escapeRegExp(filter);
+
+            this.customers.find({
+                name: {
+                    $regex: regex,
+                    $options: 'gi'
+                }
+            })
+            .toArray(
+                (err, docs) => {
+                    if (err) return reject(err);
+
+                    resolve(docs);
+                }
+            )
+        }
+    )
+}
+
+Customers.prototype.get = function (customerId) {
+    if (!customerId) return Promise.reject(new Error('customer id is required'));
+
+    return new Promise(
+        (resolve, reject) => {
+            this
+                .customers
+                .findOne({
+                    _id: customerId
+                },
+                (err, doc) => {
+                    if (err) return reject(err);
+
+                    resolve(doc);
+                });
+        }
+    )
 }
 
 Customers.prototype.create = function (name) {
@@ -17,9 +64,9 @@ Customers.prototype.create = function (name) {
                 if (err) return reject(err);
 
                 resolve(id);
-            })
+            });
         }
-    )
-}
+    );
+};
 
 module.exports = Customers;
--- test/api/customers.js
+++ test/api/customers.js
@@ -2,6 +2,7 @@ const Db = require('tingodb')({ memStore: true }).Db;
 const assert = require('assert');
 const EventEmitter = require('events').EventEmitter;
 const Customers = require('../../api/customers');
+const async = require('async');
 
 describe('Customers', () => {
     var db;
@@ -15,6 +16,121 @@ describe('Customers', () => {
         db = new Db('test', {});
 
         target = new Customers(db, bus);
+    });
+
+    describe('.search', () => {
+        beforeEach((done) => {
+            require('async')
+                .series(
+                [
+                    (callback) => db
+                        .collection('customers')
+                        .insert(
+                        {
+                            _id: 'customer1',
+                            name: 'John Doe'
+                        }, (err) => callback(err)),
+                    (callback) => db
+                        .collection('customers')
+                        .insert(
+                        {
+                            _id: 'customer2',
+                            name: 'Mary Doe'
+                        }, (err) => callback(err))
+                ],
+                (err) => done(err)
+                );
+        });
+
+        it('requires filter', (done) => {
+            target
+                .search()
+                .then(
+                () => done(new Error()),
+                (err) => {
+                    try {
+                        assert(err);
+                        assert(err instanceof Error);
+                        assert.equal(err.message, 'filter is required');
+
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                }
+                );
+        });
+
+        it('resolves filtered result', (done) => {
+            target
+                .search('Mary')
+                .then(
+                (results) => {
+                    try {
+                        assert(results);
+                        assert.deepEqual(results, [{
+                            _id: 'customer2',
+                            name: 'Mary Doe'
+                        }])
+
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                }
+                );
+        });
+    });
+
+    describe('.get', () => {
+        beforeEach((done) => {
+            db
+                .collection('customers')
+                .insert(
+                {
+                    _id: 'customer1',
+                    name: 'John Doe'
+                },
+                (err) => done(err)
+                );
+        });
+
+        it('requires customer id', (done) => {
+            target
+                .get()
+                .then(
+                () => done(new Error()),
+                (err) => {
+                    try {
+                        assert(err);
+                        assert(err instanceof Error);
+                        assert.equal(err.message, 'customer id is required');
+
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                }
+                );
+        });
+
+        it('resolves customer', (done) => {
+            target
+                .get('customer1')
+                .then(
+                (customer) => {
+                    try {
+                        assert(customer);
+                        assert.equal(customer._id, 'customer1');
+                        assert.equal(customer.name, 'John Doe');
+
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                }
+                )
+        });
     });
 
     describe('.create', () => {
